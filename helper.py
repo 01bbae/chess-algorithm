b52b0457ca700cea39facdd77aaac2c1325e1d29
Raymond Yan <lyan@chapman.edu>
2023-03-27 21:03:22 -0700

    Implementation of Zobrist Hashing Algorithm Template
    the basic algorithm template is implemented, but need to work on the connection with the main program and debugging

--- helper.py
+++ helper.py
@@ -1,39 +1,64 @@
 import chess
 import random
 
+
+
+num_rows = 8
+num_columns = 8
+num_squares = num_rows * num_columns
+type_pieces = {
+    "white_pawn" : 1, 
+    "white_rook" : 2, 
+    "white_knight" : 3, 
+    "white_bishop" : 4, 
+    "white_queen" : 5, 
+    "white_king" : 6, 
+    "black_pawn" : 7, 
+    "black_rook" : 8, 
+    "black_knight" : 9, 
+    "black_bishop" : 10, 
+    "black_queen" : 11, 
+    "black_king" : 12
+}
+
 def init_zobrist():
     table = []
-    for i in range(64):
-        for j in range(12):
-            table[i][j] = random.getrandbits(1)
-    black_to_move = random.getrandbits(1)
-def hashing(board):
-    # Taken from Zobrist Hashing Wikipedia Page
-    hash = 0
-    if board.turn == chess.BLACK:
-        h = h ^ 
+    for i in range(num_rows):
+        for j in range(num_columns):
+            for k in range(len(type_pieces)):
+                table[i][j][k] = random.getrandbits(1)
+    return table 
+
+def hashing():
+    board = get_board()
+    zob_table = get_zobrist_table()
+
+    h = 0
+    for i in range(num_rows):
+        for j in range(num_columns):
+            piece = get_piece(board, i, j)
+            if (piece != empty):
+                h ^= zob_table[i][j][piece]
+    return h
 
 
-# constant indices
-#     white_pawn := 1
-#     white_rook := 2
-#     # etc.
-#     black_king := 12
+#FIXME
+#utility functions that need to be modified to work with the main program
+def get_zobrist_table():
+    #return zobrist table generated from init_zobrist()
+    return None
 
-# function init_zobrist():
-#     # fill a table of random numbers/bitstrings
-#     table := a 2-d array of size 64×12
-#     for i from 1 to 64:  # loop over the board, represented as a linear array
-#         for j from 1 to 12:      # loop over the pieces
-#             table[i][j] := random_bitstring()
-#     table.black_to_move = random_bitstring()
+def get_board():
+    #return the chessboard from the main program
+    return None
 
-# function hash(board):
-#     h := 0
-#     if is_black_turn(board):
-#         h := h XOR table.black_to_move
-#     for i from 1 to 64:      # loop over the board positions
-#         if board[i] ≠ empty:
-#             j := the piece at board[i], as listed in the constant indices, above
-#             h := h XOR table[i][j]
-#     return h
+def get_piece(board, row_id, col_id):
+    #get piece name on a specific square on board, or return "empty"
+    return board[row_id][col_id]
+
+
+
+
+#credit:
+#https://en.wikipedia.org/wiki/Zobrist_hashing
+#https://www.geeksforgeeks.org/minimax-algorithm-in-game-theory-set-5-zobrist-hashing/
